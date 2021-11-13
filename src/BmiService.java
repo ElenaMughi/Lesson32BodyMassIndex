caf8a3393acb5e767c4b4947c13602d2287d6742
ElenaMughi <elena.mughi@gmail.com>
2021-11-13 15:36:50 +0300

    Fix var

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,10 +1,11 @@
 public class BmiService {
 
-    public float calculate(int Mass, int Height) {
-        int K = Height*Height;
-        float M = Mass * 10000_00 / K;
-        float  Bmi = M / 100;
-        return Bmi;
+    public float calculate(int mass, int height) {
+        int k = height*height;
+        float m = mass * 10000_00 / k;
+        float  bmi = m / 100;
+        return bmi;
     }
 
 }
+
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
 
-        int Mass = 60; //Масса в килограммах
-        int Height = 160; //Рост в см
+        int mass = 60; //Масса в килограммах
+        int height = 160; //Рост в см
 
-        float Bmi = service.calculate(Mass, Height);
+        float bmi = service.calculate(mass, height);
 
-        System.out.println("Body Mass Index is " + Bmi);
+        System.out.println("Body Mass Index is " + bmi);
     }
 }
